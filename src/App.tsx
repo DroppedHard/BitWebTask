fea09eae7a22e49fb3b70bf495bba8b968f0357c
DroppedHard <szyper7@gmail.com>
2023-11-24 20:27:49 +0100

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import NobelStorage from './data/NobelStorage';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   // const 
@@ -9,8 +10,14 @@ function App() {
 
   return (
     <>
+    <header>
       <h1>Witamy w spisie nagród Nobla </h1>
-      {/* <button onClick={() => console.log(dataHandler.data)}>get data</button> */}
+      <nav>
+        <Link to={"/test"}>test</Link>
+      </nav>
+    </header>
+    <Outlet />
+    {/* <button onClick={() => console.log(dataHandler.data)}>get data</button> */}
     </>
   )
 }
